6af684048958aa4fe36a4981850fbf2839cf57c1
Yahav N. Hoffman <ynhoffman@gmail.com>
2017-11-27 22:36:13 +0200

    Adding the FreddieAI C# Project from local driver

---
+++ FreddieAI/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace FreddieAI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(12, 12);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtContent.Size = new System.Drawing.Size(358, 347);
+            this.txtContent.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 365);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 50);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(267, 366);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(100, 50);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 428);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtContent);
+            this.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "FreddieAI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtContent;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
+
---
+++ FreddieAI/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace FreddieAI
+{
+    public partial class Form1 : Form
+    {
+        // Form Decleration...
+        SpeechSynthesizer ss = new SpeechSynthesizer();
+        PromptBuilder pb = new PromptBuilder();
+        SpeechRecognitionEngine sre = new SpeechRecognitionEngine();
+        Choices clist = new Choices();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Start ( button_click )
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+
+            // List of known words
+            clist.Add(new string[] { "hello", "how are you", "what is the current time", "open chrome", "thank you", "close" });
+
+            // Creating a Grammer understaning
+            Grammar gr = new Grammar(new GrammarBuilder(clist));
+
+            try
+            {
+                sre.RequestRecognizerUpdate();
+                sre.LoadGrammar(gr);
+                sre.SpeechRecognized += Sre_SpeechRecognized;
+                sre.SetInputToDefaultAudioDevice();
+                sre.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+        }
+
+        private void Sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            
+            switch (e.Result.Text.ToString())
+            {
+                case "hello":
+                    ss.SpeakAsync("hello Maker");
+                    break;
+                case "how are you":
+                    ss.SpeakAsync("i am  doing great  Maker how about you");
+                    break;
+                case "what is the current time":
+                    ss.SpeakAsync("the current time is " + DateTime.Now.ToLongTimeString());
+                    break;
+                case "thank you":
+                    ss.SpeakAsync("pleasure is mine Maker");
+                    break;
+                case "open chrom":
+                    Process.Start("chrome", "https://www.google.com");
+                    break;
+                case "close":
+                    Application.Exit();
+                    break;
+            }
+            txtContent.Text += e.Result.Text.ToString() + Environment.NewLine;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            // Stop ( button_click )
+            sre.RecognizeAsyncStop();
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+    }
+        
+}
+
